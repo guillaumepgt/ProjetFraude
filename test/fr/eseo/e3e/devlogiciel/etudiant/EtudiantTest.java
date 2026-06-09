76ddc6efb18fefc7527e72a50c302ac30a73e94e
Titouuuuuu <titouanpeloin@gmail.com>
2026-06-09 16:14:05 +0200

    update cahierTests

--- test/fr/eseo/e3e/devlogiciel/etudiant/EtudiantTest.java
+++ test/fr/eseo/e3e/devlogiciel/etudiant/EtudiantTest.java
@@ -2,8 +2,7 @@ package fr.eseo.e3e.devlogiciel.etudiant;
 
 import org.junit.jupiter.api.Test;
 import static fr.eseo.e3e.devlogiciel.etudiant.Etudiant.Cursus.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class EtudiantTest {
 
@@ -21,5 +20,16 @@ public class EtudiantTest {
         assertEquals("Dupont", e2.getNom(), "Échec : le nom n'a pas été correctement assigné.");
         assertEquals("2024", e2.getId(), "Échec : l'ID n'a pas été correctement assigné.");
         assertEquals(E2, e2.getCursus(), "Échec : le cursus n'a pas été correctement assigné.");
+    }
+
+    @Test
+    public void testEqualsEtHashCode() {
+        Etudiant e1 = new Etudiant("Peloin", "Titouan", "111", E3e);
+        Etudiant e2 = new Etudiant("Clone", "Clone", "111", E2);
+        Etudiant e3 = new Etudiant("Peloin", "Titouan", "222", E3e);
+
+        assertEquals(e1, e2, "Deux étudiants avec le même ID doivent être égaux.");
+        assertEquals(e1.hashCode(), e2.hashCode(), "Le hashcode doit être identique pour le même ID.");
+        assertNotEquals(e1, e3, "Deux étudiants avec des ID différents ne sont pas égaux.");
     }
 }
